a0cb84fef4f2d3a6d3331b065c8a0f4578d0f7ac
de-cecco <145263265+de-cecco@users.noreply.github.com>
2023-09-17 14:58:09 +0300

    Add files via upload

---
+++ Kim254.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.kim254;
+
+/**
+ *
+ * @author nyaga
+ */
+public class Kim254 {
+
+    public static void main(String[] args) {
+     System.out.println("**************Adamson Computers Ltd***************");
+     System.out.println("#ACS Building Gilmore Ave.New Mla..Quezon City * Tell.No:123-4567");
+     System.out.println("                VAT REG.TIN 098-765-4321-0000");
+     System.out.println("                       SALES INVOICE                ");
+     System.out.println(" ");
+     System.out.println(" ");
+     System.out.println("Sold to:c026-01-1047/2021                           Date:17/9/2023");
+     System.out.println("Address:kahuria.brette@students.dkut.ac.ke          Contact Number:0100100100");
+     System.out.println(" ");
+     System.out.println(" ");
+     System.out.println("=============================================================================");
+     System.out.println("Qty        |      Item Description     |       Unit Price      |Amount");
+     System.out.println("=============================================================================");
+     System.out.println("4          |      Pencil               |       20              |80.00");
+     System.out.println("-----------------------------------------------------------------------------");
+     System.out.println("10         |      Duster               |       50              |500.00");
+     System.out.println("-----------------------------------------------------------------------------");
+     System.out.println("9          |      Pens                 |       30              |270.00");
+     System.out.println("-----------------------------------------------------------------------------");
+     System.out.println("10         |      Crayon               |       80              |800.00");
+     System.out.println("=============================================================================");
+     System.out.println("                                              |Sub Total       |1650.00");
+     System.out.println("                                              ===============================");
+     System.out.println("                                             |VAT(16%)             |264.00");
+     System.out.println("                                              ===============================");
+     System.out.println("                                             |Total Amount    |1914.00");
+     System.out.println("                                              ===============================");
+    }
+}
